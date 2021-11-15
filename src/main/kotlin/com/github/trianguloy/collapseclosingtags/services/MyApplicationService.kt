6583c6647b2579bf2dc1edfcee624bd69d36b4d6
GitHub Action <action@github.com>
2021-11-15 16:54:59 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/trianguloy/collapseclosingtags/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.trianguloy.collapseclosingtags.services
+
+import com.github.trianguloy.collapseclosingtags.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/trianguloy/collapseclosingtags/services/MyProjectService.kt
+++ src/main/kotlin/com/github/trianguloy/collapseclosingtags/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.trianguloy.collapseclosingtags.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.trianguloy.collapseclosingtags.MyBundle
 
 class MyProjectService(project: Project) {
 
